ac763d0381fa7afde028644e812591ed24686fe8
Mikhail Jefferson <mikhailtrabalho@gmail.com>
2022-09-07 14:00:19 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 
-import Header from "./components/NotificationButton/Header"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/NotificationButton/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
@@ -20,4 +20,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
